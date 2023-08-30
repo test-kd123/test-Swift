5676108ece02ac0591cdacca11ccac560b25889c
tangchao <tangchao@kdanmobile.com>
2023-08-30 11:44:37 +0800

    【综合】去除授权信息

--- MyApiLibraryDemo/samples/PDFSplit.swift
+++ MyApiLibraryDemo/samples/PDFSplit.swift
@@ -7,8 +7,8 @@
 
 import Cocoa
 
-private let public_key = "public_key_923a61e724db57c4f6706660a8121e6a"
-private let secret_key = "secret_key_a3fd33db0ca1901688bd1582df08ac70"
+private let public_key = "x"
+private let secret_key = "x"
 class PDFSplit: NSObject {
     private static var client: CPDFClient = CPDFClient(publicKey: public_key, secretKey: secret_key)
     
